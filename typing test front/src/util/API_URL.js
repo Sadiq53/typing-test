d3940bdb16f74f24724c570e4c773dec1470630f
Sadiq53 <jafarussadiq098@gmail.com>
2024-10-21 17:09:34 +0530

    add vercel file

--- typing test front/src/util/API_URL.js
+++ typing test front/src/util/API_URL.js
@@ -1,4 +1,6 @@
-const ADMIN_API_URL = "http://localhost:8080/admin";
-const USER_API_URL = "http://localhost:8080/user";
+// const ADMIN_API_URL = "http://localhost:8080/admin";
+const ADMIN_API_URL = "https://typing-test-server-z0yx.onrender.com/admin";
+// const USER_API_URL = "http://localhost:8080/user";
+const USER_API_URL = "https://typing-test-server-z0yx.onrender.com/user";
 
 export { ADMIN_API_URL, USER_API_URL };
