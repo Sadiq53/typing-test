7eb214352738bd776bc102397d66176b147e190c
Sadiq53 <jafarussadiq098@gmail.com>
2024-10-23 22:38:20 +0530

    moderate level updates

--- typing test front/src/util/API_URL.js
+++ typing test front/src/util/API_URL.js
@@ -1,7 +1,8 @@
-const ADMIN_API_URL = "http://localhost:8080/admin";
-// const ADMIN_API_URL = "https://typing-test-server-z0yx.onrender.com/admin";
-const USER_API_URL = "http://localhost:8080/user";
-// const USER_API_URL = "https://typing-test-server-z0yx.onrender.com/user";
-const BASE_API_URL = "http://localhost:8080";
+// const ADMIN_API_URL = "http://localhost:8080/admin";
+const ADMIN_API_URL = "https://typing-test-server-z0yx.onrender.com/admin";
+// const USER_API_URL = "http://localhost:8080/user";
+const USER_API_URL = "https://typing-test-server-z0yx.onrender.com/user";
+// const BASE_API_URL = "http://localhost:8080";
+const BASE_API_URL = "https://typing-test-server-z0yx.onrender.com";
 
 export { ADMIN_API_URL, USER_API_URL, BASE_API_URL };
