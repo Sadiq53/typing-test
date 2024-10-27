604017f091df1b8cb95ad41a8bc35e0c14429f35
Sadiq53 <jafarussadiq098@gmail.com>
2024-10-27 16:13:46 +0530

    added the admin add delete update paragraphs functionality

--- typing test front/src/modules/UserModule.jsx
+++ typing test front/src/modules/UserModule.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useNavigate } from 'react-router-dom'
 import { handleGetUserData } from '../redux/UserDataSlice';
